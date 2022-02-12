53ef9842d4fe2da922125b9d2440889102dfe324
alexgabriel28 <alexander.gabriel@rwth-aachen.de>
2022-02-12 21:06:31 +0000

    Change setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     author = "Alexander Gabriel",
     description = "Package MT @ ITA RWTH - Continual Learning x Composite Prod",
-    name = "master-thesis",
+    name = "thesis",
     version = "0.1.0",
     packages = find_packages(include =["master-thesis", "master-thesis.*"]),
 )
