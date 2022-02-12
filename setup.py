a4b0076f0ff8eb7d2036ca858e52942885959f98
alexgabriel28 <alexander.gabriel@rwth-aachen.de>
2022-02-12 20:05:29 +0000

    Restructure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_package
+
+setup(
+    author = "Alexander Gabriel"
+    description = "Master Thesis <> Continual Learning in Composite Production",
+    name = "master-thesis"
+    version = "0.1.0"
+    packages = find_packages(include = ["master-thesis", "master-thesis.*"]),
+)
